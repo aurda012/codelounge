89c09c8c3f1fa29b2248d7400c96b0e5ff8aa694
Alfredo Urdaneta <alfredourdaneta@Alfredos-MacBook-Pro.local>
2024-04-29 19:59:46 -0400

    Added local search bar for home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,7 +3,7 @@
 // import Filter from "@/components/shared/Filter";
 // import NoResult from "@/components/shared/NoResult";
 // import Pagination from "@/components/shared/Pagination";
-// import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
+import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import {
@@ -51,12 +51,12 @@ export default async function Home({ searchParams }: SearchParamsProps) {
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center ">
         <h1 className="h1-bold text-dark100_light900">All Questions</h1>
         <Link href={`/ask-question`} className="flex justify-end max-sm:w-full">
-          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900 rounded-[8px]">
             Ask a Question
           </Button>
         </Link>
       </div>
-      {/* <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
         <LocalSearchBar
           route="/"
           iconPosition="left"
@@ -64,12 +64,12 @@ export default async function Home({ searchParams }: SearchParamsProps) {
           placeholder="Search for questions"
           otherClasses="flex-1"
         />
-        <Filter
+        {/* <Filter
           filters={HomePageFilters}
           otherClasses="min-h-[56px] sm:min-w-[170px]"
           containerClasses="hidden max-md:flex"
-        />
-      </div> */}
+        /> */}
+      </div>
       {/* <HomeFilters /> */}
       {/* <div className="mt-10 flex w-full flex-col gap-6 ">
         {result.questions.length > 0 ? (
