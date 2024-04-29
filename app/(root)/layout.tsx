15e92ef03bf5b6db01c7a0ef70e8322a0e3aa6ab
Alfredo Urdaneta <alfredourdaneta@Alfredos-MacBook-Pro.local>
2024-04-29 19:30:14 -0400

    Added left sidebar for layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,7 +2,7 @@
 
 import DarkModeSwitcher from "@/components/shared/dark-mode-switcher";
 import NavBar from "@/components/shared/navbar/NavBar";
-// import LeftSideBar from "@/components/shared/sidebar/LeftSideBar";
+import LeftSideBar from "@/components/shared/sidebar/LeftSideBar";
 // import RightSideBar from "@/components/shared/sidebar/RightSideBar";
 import { Toaster } from "@/components/ui/toaster";
 import { ClerkProvider } from "@clerk/nextjs";
@@ -14,7 +14,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
       <main className="background-light850_dark100 relative">
         <NavBar />
         <div className="flex">
-          {/* <LeftSideBar /> */}
+          <LeftSideBar />
           <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
             <div className="mx-auto w-full max-w-5xl">{children}</div>
           </section>
