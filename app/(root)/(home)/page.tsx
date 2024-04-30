76972929469e17aae6b80019b5659c14fe196d6f
Alfredo Urdaneta <alfredourdaneta@Alfredos-MacBook-Pro.local>
2024-04-29 20:13:43 -0400

    Added placeholder for home page no results

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
 import Link from "next/link";
 import { auth } from "@clerk/nextjs/server";
-// import QuestionCard from "@/components/cards/QuestionCard";
-// import NoResult from "@/components/shared/NoResult";
-// import Pagination from "@/components/shared/Pagination";
 import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
 import Filter from "@/components/shared/Filter";
 import HomeFilters from "@/components/home/HomeFilters";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
+// import QuestionCard from "@/components/cards/QuestionCard";
+import NoResult from "@/components/shared/NoResult";
+// import Pagination from "@/components/shared/Pagination";
 import {
   getQuestions,
   getRecommendedQuestions,
@@ -71,32 +71,33 @@ export default async function Home({ searchParams }: SearchParamsProps) {
         />
       </div>
       <HomeFilters />
-      {/* <div className="mt-10 flex w-full flex-col gap-6 ">
+      <div className="mt-10 flex w-full flex-col gap-6 ">
         {result.questions.length > 0 ? (
-          result.questions.map((question) => (
-            <QuestionCard
-              key={question._id}
-              tags={question.tags}
-              title={question.title}
-              author={question.author}
-              upvotes={question.upvotes.length}
-              answers={question.answers}
-              views={question.views}
-              _id={question._id}
-              createdAt={question.createdAt}
-            />
-          ))
+          <></>
         ) : (
+          // result.questions.map((question) => (
+          //   <QuestionCard
+          //     key={question._id}
+          //     tags={question.tags}
+          //     title={question.title}
+          //     author={question.author}
+          //     upvotes={question.upvotes.length}
+          //     answers={question.answers}
+          //     views={question.views}
+          //     _id={question._id}
+          //     createdAt={question.createdAt}
+          //   />
+          // ))
           <NoResult
             title="No Questions Found"
             description="Be the first to break the silence! 🚀 Ask a Question and kickstart the
-          discussion. our query could be the next big thing others learn from. Get
+          discussion. Your question could be the next big thing others learn from. Get
           involved! 💡"
             link="/ask-question"
             linkTitle="Ask a Question"
           />
         )}
-      </div> */}
+      </div>
       <div className="mt-10">
         {/* <Pagination
           pageNumber={searchParams?.page ? +searchParams.page : 1}
